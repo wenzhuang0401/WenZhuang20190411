e50480a5505e3e19b98b4405c729b1d15f47d778
wenzhuang0401 <wenzhuang0401@yeah.net>
2019-04-11 08:50:50 +0800

    Initial commit

---
+++ app/src/main/java/weektest/baway/com/wenzhuang20190411/MainActivity.java
@@ -0,0 +1,14 @@
+package weektest.baway.com.wenzhuang20190411;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String a="这是一行代码";
+    }
+}
